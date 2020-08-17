4ed6baba72681d5c7f29e6b70d0a0f1834e4dd28
Kazuyuki Matsuda <kazm@shorindo.com>
2020-08-17 18:55:22 +0900

    working...

--- src/test/java/com/shorindo/tools/RhinoTerminal.java
+++ src/test/java/com/shorindo/tools/RhinoTerminal.java
@@ -7,24 +7,24 @@ import java.io.PrintStream;
 import org.mozilla.javascript.tools.shell.Main;
 
 public class RhinoTerminal {
-	public static void main(String[] args) {
-		try {
-			PipedInputStream sin = new PipedInputStream();
-			PipedOutputStream sout = new PipedOutputStream(sin);
-			PrintStream ps = new PrintStream(sout);
-			PipedInputStream kin = new PipedInputStream();
-			PipedOutputStream kout = new PipedOutputStream(kin);
-			
-			Terminal terminal = new Terminal("UTF-8", 80, 25);
-			terminal.open();
-			terminal.connect(sin, kout);
+    public static void main(String[] args) {
+        try {
+            PipedInputStream sin = new PipedInputStream();
+            PipedOutputStream sout = new PipedOutputStream(sin);
+            PrintStream ps = new PrintStream(sout);
+            PipedInputStream kin = new PipedInputStream();
+            PipedOutputStream kout = new PipedOutputStream(kin);
 
-			Main.setIn(kin);
-			Main.setOut(ps);
-			Main.setErr(ps);
-			Main.main(new String[]{});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            Terminal terminal = new Terminal("UTF-8", 80, 25);
+            terminal.open();
+            terminal.connect(sin, kout);
+
+            Main.setIn(kin);
+            Main.setOut(ps);
+            Main.setErr(ps);
+            Main.main(new String[] {});
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
